d6083d94f0c23ec60b43a0d95aa004320533ed69
neepunraj <neepushre97@gmail.com>
2025-02-05 19:41:24 +0100

    javascript excercise

---
+++ javascript/javascript1/week1/homework/startupNameGenerator.js
@@ -0,0 +1,35 @@
+console.log("===Startup Name Generator===")
+const firstWords = [
+  "Alpha",
+  "Beta",
+  "Cargo",
+  "Dango",
+  "Echo",
+  "FisherMan",
+  "Gokyo",
+  "Helicoptor",
+  "InkMan",
+  "Romeo",
+];
+const seconWords = [
+  "Floppy",
+  "Luck",
+  "Star",
+  "Mango",
+  "Friendly",
+  "Ship",
+  "Trek",
+  "Smash",
+  "Rider",
+  "Juliet",
+];
+
+function getRandomNumber() {
+  const randomNumber = Math.floor(Math.random() * 10);
+  return randomNumber;
+}
+const startupName =
+  firstWords[getRandomNumber()] + seconWords[getRandomNumber()];
+console.log(
+  `Your name for Startup Company is ${startupName}, and it is ${startupName.length} charachters long`
+);
