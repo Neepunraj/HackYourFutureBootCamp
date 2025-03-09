e79673db20f673fc4e9ecc89f1c751e8cd3bda7a
Neepun Raj Shrestha <neepushre97@gmail.com>
2025-03-09 09:12:12 +0100

    Update javascript/javascript2/week1/homeWork/stringsEndsWith.js
    Co-authored-by: Valentin Daguenet <5467758+vdaguenet@users.noreply.github.com>

--- javascript/javascript2/week1/homeWork/stringsEndsWith.js
+++ javascript/javascript2/week1/homeWork/stringsEndsWith.js
@@ -2,7 +2,8 @@ function solution(text, ending) {
   let array = text.split("");
   array = array.slice();
   console.log(array);
-  return (stringMEthod = text.endsWith(ending));
+  const stringMethod = text.endsWith(ending);
+  return stringMethod;
 }
 console.log(solution("abc", "bc")); // returns true
 console.log(solution("abc", "d")); // returns false
