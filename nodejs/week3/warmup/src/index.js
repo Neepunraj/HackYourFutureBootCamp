fe0c4eafd0b9d4f86549d0a73d681621bcbcd9ae
Neepun Raj Shrestha <neepushre97@gmail.com>
2025-05-29 16:05:43 +0200

    warmup

--- nodejs/week3/warmup/src/index.js
+++ nodejs/week3/warmup/src/index.js
@@ -1,12 +1,12 @@
 import express from 'express'
 import dotenv from 'dotenv'
-import { appRouter } from './routes/mealsRoute.js'
+import { appRouter } from './routes/contactsRoutes.js'
 dotenv.config()
 const app = express()
 const port = process.env.PORT || 8000
 app.use(express.json())
 
-app.use(appRouter)
+app.use('/api/contacts', appRouter)
 app.get('/', (req, res) => {
     res.send("Hello this is search Engine")
 })
--- nodejs/week3/warmup/src/routes/mealsRoute.js
+++
@@ -1,7 +0,0 @@
-import express from 'express'
-import { getDoccumnetsbyID, getMealSearch, SeachPostMethod } from '../controllers/getMeals.js'
-
-export const appRouter = express.Router()
-appRouter.get('/search', getMealSearch)
-appRouter.get('/doccuments/:id', getDoccumnetsbyID)
-appRouter.post('/search', SeachPostMethod)
