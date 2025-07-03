1810f35e212983d36fb736381885537215f89be4
Neepun Raj Shrestha <neepushre97@gmail.com>
2025-07-03 12:46:33 +0200

    use reducer implemented

--- react/react2/week1/homework/src/app/layout.js
+++ react/react2/week1/homework/src/app/layout.js
@@ -2,6 +2,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/context/themeContext";
+import { CounterContextProvider } from "@/context/couterReducerContext";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,10 +26,13 @@ export default function RootLayout({ children }) {
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <ThemeProvider>
-          <main>
+          <CounterContextProvider>
+
 
             {children}
-          </main>
+
+
+          </CounterContextProvider>
         </ThemeProvider>
       </body>
     </html>
--- react/react2/week1/homework/src/app/page.js
+++ react/react2/week1/homework/src/app/page.js
@@ -1,13 +1,14 @@
 "use client"
 import { useTheme } from "@/context/themeContext";
+import Counter from "./components/counter";
 
 export default function Home() {
   const { isDark, toggleTheme } = useTheme()
   return (
-    <div className={`w-full min-h-screen ${isDark ? "bg-black text-white" : "bg-white"}`}>
+    <div className={`w-full min-h-screen items-center flex flex-col ${isDark ? "bg-black text-white" : "bg-white"}`}>
       <h1>Hello, this is theme toggling demo in root page  </h1>
       <button onClick={toggleTheme} className={`${isDark ? "text-white " : ""} border rounded p-2`}>Toggle Theme</button>
-
+      <Counter />
     </div>
   );
 }
