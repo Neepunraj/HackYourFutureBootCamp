caf699be4111dca6dfe9ec787bd5f1b45efcab9b
Neepun Raj Shrestha <neepushre97@gmail.com>
2025-07-03 08:10:50 +0200

    context Api HW

--- react/react2/week1/homework/src/app/layout.js
+++ react/react2/week1/homework/src/app/layout.js
@@ -1,5 +1,7 @@
+
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { ThemeProvider } from "@/context/themeContext";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -22,7 +24,12 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <ThemeProvider>
+          <main>
+
+            {children}
+          </main>
+        </ThemeProvider>
       </body>
     </html>
   );
